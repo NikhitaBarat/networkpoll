5e0fb9ae3488e965469b02b3627c14e6df86ebd0
Siddhant Prateek <siddhantprateek@gmail.com>
2022-10-20 22:09:30 +0530

    voter contract

---
+++ contracts/VoterContract.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+contract VoterContract {
+
+  // mapping with voter id and public key
+  // mapping with voteer id and voter's address
+  // 
+  constructor() {
+  }
+}
