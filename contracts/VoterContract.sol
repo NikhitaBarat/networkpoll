84909f65c3592b3856e86bc1f92baf3259ef65ca
Siddhant Prateek <siddhantprateek@gmail.com>
2022-10-22 12:36:43 +0530

    get candidate votes, mint, afterTransfer function added

--- contracts/VoterContract.sol
+++ contracts/VoterContract.sol
@@ -1,47 +1,60 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.15;
 
-
-// import ERC20 
+// import ERC20Votes
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 // import "@openzeppelin/contracts/token/ERC20/extensions/ERC20VotesComp.sol";
 
-
-
 abstract contract VoterContract is ERC20Votes {
+    // mapping with voter id and public key
+    mapping(address => string) public VoterPubKeyList;
+
+    // mapping with voteer id and voter's address
+    mapping(string => address) public linkedVoterId;
+
+    // transfer coin to voter
+
+    // transfer coin to candidate
+    constructor() ERC20Votes() {}
 
 
-  // mapping with voter id and public key
-  mapping(address => string) public VoterPubKeyList;
+    // ERC20Votes Consists:
 
-  // mapping with voteer id and voter's address
-  mapping(string => address) public linkedVoterId;
+    // checkpoints(account, pos)
+    // numCheckpoints(account)
+    // delegates(account)
 
-  // transfer coin to voter
+    // getVotes(account)
+    function getCandidateVotes(address candidateAccount) external view {
+        // VoterPubKeyList[candidate] = "0xB...JKLAS87JHKSA8H";
+      require(candidateAccount != address(0), 'Error - Candidate account cannot be zero address');
+      getVotes(candidateAccount);        
+    }
 
-  // transfer coin to candidate
-  constructor() ERC20Votes() {
 
-  }
-  
-  function getCandidateVotes(address candidate) public {
-    VoterPubKeyList[candidate] = "0xB...JKLAS87JHKSA8H";
-  }
-  
-  // ERC20Votes Consists:
+    // getPastVotes(account, blockNumber)
 
-  // checkpoints(account, pos)
-  // numCheckpoints(account)
-  // delegates(account)
-  // getVotes(account)
-  // getPastVotes(account, blockNumber)
-  // getPastTotalSupply(blockNumber)
-  // delegate(delegatee)
-  // delegateBySig(delegatee, nonce, expiry, v, r, s)
-  // _maxSupply()
-  // _mint(account, amount)
-  // _burn(account, amount)
-  // _afterTokenTransfer(from, to, amount)
-  // _delegate(delegator, delegatee)
+    // getPastTotalSupply(blockNumber)
+    
+    // delegate(delegatee)
+    
+    // delegateBySig(delegatee, nonce, expiry, v, r, s)
+    
+    // _maxSupply()
+    
+    // _mint(account, amount)
+    function mint(address to, uint256 amount) external {
+      _mint(to, amount);
+    }
 
+    // _burn(account, amount)
+
+
+    // _afterTokenTransfer(from, to, amount)
+    function afterTokenTransfer(address _from, address _to, uint256 amount) external {
+      require(_to != address(0), 'Error - ERC20 transfer to zero address');
+      require(amount != 0, 'Error - ERC20 cannot transfer zero amount');
+      _afterTokenTransfer(_from, _to, amount);
+    }
+    // _delegate(delegator, delegatee)
 }
