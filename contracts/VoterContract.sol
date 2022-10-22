9c50006627b56549b56cb3b2aa83fb92ff5be696
Siddhant Prateek <siddhantprateek@gmail.com>
2022-10-22 10:55:50 +0530

    inherit ERC20Votes contract

--- contracts/VoterContract.sol
+++ contracts/VoterContract.sol
@@ -1,11 +1,47 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.15;
 
-contract VoterContract {
+
+// import ERC20 
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+// import "@openzeppelin/contracts/token/ERC20/extensions/ERC20VotesComp.sol";
+
+
+
+abstract contract VoterContract is ERC20Votes {
+
 
   // mapping with voter id and public key
+  mapping(address => string) public VoterPubKeyList;
+
   // mapping with voteer id and voter's address
-  // 
-  constructor() {
+  mapping(string => address) public linkedVoterId;
+
+  // transfer coin to voter
+
+  // transfer coin to candidate
+  constructor() ERC20Votes() {
+
   }
+  
+  function getCandidateVotes(address candidate) public {
+    VoterPubKeyList[candidate] = "0xB...JKLAS87JHKSA8H";
+  }
+  
+  // ERC20Votes Consists:
+
+  // checkpoints(account, pos)
+  // numCheckpoints(account)
+  // delegates(account)
+  // getVotes(account)
+  // getPastVotes(account, blockNumber)
+  // getPastTotalSupply(blockNumber)
+  // delegate(delegatee)
+  // delegateBySig(delegatee, nonce, expiry, v, r, s)
+  // _maxSupply()
+  // _mint(account, amount)
+  // _burn(account, amount)
+  // _afterTokenTransfer(from, to, amount)
+  // _delegate(delegator, delegatee)
+
 }
