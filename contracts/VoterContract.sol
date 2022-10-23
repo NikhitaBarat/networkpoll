a11e75a65e13068bbf36cf0a030b43e7828c8580
Siddhant Prateek <siddhantprateek@gmail.com>
2022-10-24 02:33:47 +0530

    Voter contract need fixes

--- contracts/VoterContract.sol
+++ contracts/VoterContract.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.15;
+pragma solidity ^0.8.0;
 
 // import ERC20Votes
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
@@ -25,7 +25,7 @@ abstract contract VoterContract is ERC20Votes {
     // delegates(account)
 
     // getVotes(account)
-    function getCandidateVotes(address candidateAccount) external view {
+    function getCandidateVotes(address candidateAccount) public view {
         // VoterPubKeyList[candidate] = "0xB...JKLAS87JHKSA8H";
       require(candidateAccount != address(0), 'Error - Candidate account cannot be zero address');
       getVotes(candidateAccount);        
@@ -39,11 +39,13 @@ abstract contract VoterContract is ERC20Votes {
     // delegate(delegatee)
     
     // delegateBySig(delegatee, nonce, expiry, v, r, s)
-    
+    function delegateBySignature(address candidate, uint256 nonce, uint256 expiry, uint8 v, bytes32 r, bytes32 s) public {
+      delegateBySig(candidate, nonce, expiry, v, r, s);
+    }
     // _maxSupply()
     
     // _mint(account, amount)
-    function mint(address to, uint256 amount) external {
+    function mint(address to, uint256 amount) public {
       _mint(to, amount);
     }
 
@@ -51,7 +53,7 @@ abstract contract VoterContract is ERC20Votes {
 
 
     // _afterTokenTransfer(from, to, amount)
-    function afterTokenTransfer(address _from, address _to, uint256 amount) external {
+    function afterTokenTransfer(address _from, address _to, uint256 amount) public {
       require(_to != address(0), 'Error - ERC20 transfer to zero address');
       require(amount != 0, 'Error - ERC20 cannot transfer zero amount');
       _afterTokenTransfer(_from, _to, amount);
