f9871a0b673b9b7918794783644504b141cc9ce2
Siddhant Prateek <siddhantprateek@gmail.com>
2022-11-08 15:23:16 +0530

    private and public keygen connected to client

--- client/src/pages/dashboard/dashboard.pages.jsx
+++ client/src/pages/dashboard/dashboard.pages.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
-import { CandidateCard } from '../../components'
+import React from 'react';
+import { useState } from 'react';
+import { CandidateCard } from '../../components';
 
 const Dashboard = () => {
+  const [jwt, setJWT] = useState()
+  let response = fetch('http://localhost:4000/jwt')
+  response.then((res) => {
+    // console.log(res)
+    return res.json()
+  })
+  .then((resp) => {
+    console.log(resp)
+    setJWT(resp.token)
+  })
+
+  let secretResponse = fetch('http://localhost:4000/secret', {  
+    headers: {
+      Authorization: `Bearer ${jwt}`
+    }
+  })
+  secretResponse.then((res) => {
+    // console.log(res)
+    return res.json()
+  })
+  .then((resp) => {
+    console.log(resp)
+  })
+
   return (
     <div className='voting-window'>
       <CandidateCard/>
