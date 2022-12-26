d7c52d903ee81299706cc0ed1b233cbcc2b08847
Nikhita <bnikhita2003@gmail.com>
2022-12-27 02:01:00 +0530

    evoting vr2 contract

--- contracts/EVotingVR2.sol
+++ contracts/EVotingVR2.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract EVoting {
+contract EVotingVR2 {
     // Struct to represent an election
     struct Election {
         uint id;
