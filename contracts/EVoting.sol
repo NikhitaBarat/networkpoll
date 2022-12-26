669f41b0616b02d9e00efdf190e3127bd97b76e1
Nikhita <bnikhita2003@gmail.com>
2022-12-27 02:00:44 +0530

    evoting contract

--- contracts/EVoting.sol
+++ contracts/EVoting.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract Election {
+contract EVoting {
   // Struct to hold information about a voter
   struct Voter {
     address addr; // Ethereum address of the voter
